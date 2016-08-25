3f84362f3c6fa14006cbc53bcdb60ad24e117b48
Ruslan Rostovtsev <rrostovtsev@ac-sw.com>
2016-08-25 10:59:22 +0700

    Added custom BIOS support

--- kernel/arch/dreamcast/hardware/cdrom.c
+++ kernel/arch/dreamcast/hardware/cdrom.c
@@ -5,6 +5,7 @@
    Copyright (C) 2000 Dan Potter
    Copyright (C) 2014 Lawrence Sebald
    Copyright (C) 2014 Donald Haase
+   Copyright (C) 2016 Ruslan Rostovtsev
 
  */
 
@@ -448,10 +449,19 @@ int cdrom_init() {
 
     /* Reactivate drive: send the BIOS size and then read each
        word across the bus so the controller can verify it. */
-    *react = 0x1fffff;
-
-    for(p = 0; p < 0x200000 / sizeof(bios[0]); p++) {
-        (void)bios[p];
+    if((*(uint16 *)0xa0000000) == 0xe6ff) {
+        
+        *react = 0x3ff;
+        for(p = 0; p < 0x400 / sizeof(bios[0]); p++) {
+            (void)bios[p];
+        }
+        
+    } else {
+        
+        *react = 0x1fffff;
+        for(p = 0; p < 0x200000 / sizeof(bios[0]); p++) {
+            (void)bios[p];
+        }
     }
 
     mutex_lock(&_g1_ata_mutex);
